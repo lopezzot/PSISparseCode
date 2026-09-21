5a1cd7d19b0dd10a088cb5d5ba6db14cff5d4a47
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2026-09-21 15:26:59 +0200

    Adjust primary parameters

--- MLSpectrometer/Spectrometer/include/SimulationConfig.hh
+++ MLSpectrometer/Spectrometer/include/SimulationConfig.hh
@@ -10,15 +10,15 @@ constexpr G4double LayerThickness = 5.0 * mm;
 constexpr G4double LayerSizeXY = 100.0 * mm;
 constexpr G4double LayerGap = 0.0 * mm;
 
-constexpr G4int NumberOfEnergyBins = 50;
+constexpr G4int NumberOfEnergyBins = 100;
 constexpr G4double EnergyMin = 0.1 * MeV;
-constexpr G4double EnergyMax = 1000.0 * MeV;
+constexpr G4double EnergyMax = 100.0 * MeV;
 
 // First training configuration: one Gaussian spectrum per shot.
-constexpr G4double GaussianMeanMin = 1.0 * MeV;
-constexpr G4double GaussianMeanMax = 9.0 * MeV;
-constexpr G4double GaussianSigmaMin = 0.1 * MeV;
-constexpr G4double GaussianSigmaMax = 0.8 * MeV;
+constexpr G4double GaussianMeanMin = 10.0 * MeV;
+constexpr G4double GaussianMeanMax = 90.0 * MeV;
+constexpr G4double GaussianSigmaMin = 1.0 * MeV;
+constexpr G4double GaussianSigmaMax = 10.0 * MeV;
 
 // Keep the first dataset statistically simple: fixed number of photons per
 // shot.
